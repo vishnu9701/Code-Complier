388b9ef388b0a34ecba31e4d60e98f49cc98795a
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2023-03-25 15:31:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from streamlit.components.v1 import iframe
+
+from streamlit_option_menu import option_menu
+st.set_page_config(    page_title="AI Resume Analyser",    page_icon='./Images/mm.png')
+    
+selected=option_menu(
+        menu_title="RUHVSoft LLP",
+        options = ["Code-Editor"],
+        icons=["code-slash"],
+        menu_icon="emoji-laughing",
+        default_index=0,
+        orientation="horizontal",)
+st.title("Select programming language")
+language = st.selectbox("", ["C", "C++","Java", "Python","HTML/CSS/Javascript","Verilog"])
+
+if language == "C":
+    components.iframe(''' http://jdoodle.com/a/63kh''',width=800, height=666, scrolling=False)
+    
+elif language == "C++":
+    components.iframe(''' https://www.jdoodle.com/a/63kl''',width=800, height=666, scrolling=False)
+    
+elif language == "Java":
+    components.iframe(''' https://www.jdoodle.com/a/63k9''',width=800, height=666, scrolling=False)
+    
+elif language == "Python":
+    components.iframe(''' https://www.jdoodle.com/a/63kn ''',width=800, height=666 ,scrolling=False)
+    
+elif language == "HTML/CSS/Javascript":
+    components.iframe(''' https://jdoodle.com/h/2Vv ''',width=800, height=1200 ,scrolling=False)
+
+elif language == "Verilog":
+    components.iframe(''' https://jdoodle.com/a/63ks''',width=800, height=666 ,scrolling=False)
